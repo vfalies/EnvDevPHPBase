9a9e5d489f7e3c94291cc3e59f02c883f414246e
Vincent Faliès <vincent.falies@gmail.com>
2017-11-26 15:50:48 +0100

    Add default message for git tag

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Push commits and launch build for all versions..."
-git tag -fa 5.6
-git tag -fa 7.0
-git tag -fa 7.1
+git tag -fa 5.6 -m 5.6
+git tag -fa 7.0 -m 7.0
+git tag -fa 7.1 -m 7.1
 
 git push --tags --force
